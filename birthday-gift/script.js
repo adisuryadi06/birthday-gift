57d91f9f9262e12ecca3d4ccce36136215b37df1
adisuryadi06 <140477152+adisuryadi06@users.noreply.github.com>
2025-07-14 11:09:07 +0700

    Delete birthday-gift directory

--- birthday-gift/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('giftBtn').addEventListener('click', () => {
-  const surprise = document.getElementById('surprise');
-  const wifeName = document.getElementById('wifeName');
-  
-  // Ganti ini dengan nama asli istri Anda
-  wifeName.textContent = "Dewi Anjani";
-
-  surprise.classList.remove('hidden');
-});
