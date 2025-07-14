8caf42817f9503eff70458ac1770b880eb693ec6
adisuryadi06 <140477152+adisuryadi06@users.noreply.github.com>
2025-07-14 10:59:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,19 @@ setInterval(() => {
   const x = e.clientX;
   const y = e.clientY;
   createFirework(x, y);
+
+  function createHearts() {
+  const heartsContainer = document.getElementById("hearts");
+
+  setInterval(() => {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = (Math.random() * 3 + 3) + "s";
+    heartsContainer.appendChild(heart);
+
+    setTimeout(() => heart.remove(), 6000);
+  }, 300);
+}
+
 });
